b94b27ff3dbac7f5683dd30c099896b48af7f2e8
ahmed-youssef-fouad <ahmedyoussef@Dark-Night.localdomain>
2023-07-02 23:14:35 +0200

    minor update to deploy shell scrip

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ echo "Local Account Id is $account_id"
 #-------------------------------------------------------------
 #The application building and deployment
 #-------------------------------------------------------------
-cd $CODEBUILD_SRC_DIR
+cd $CODEBUILD_SRC_DIR 
 echo "Run dependencies installation"
 echo "Run copy to S3 s3://mbition-webapp-demo.example.com"
-aws s3 sync dist/ s3://mbition-webapp-demo.example.com --delete
+aws s3 sync . s3://mbition-webapp-demo.example.com --delete
 echo "Clean up the cache for cloudfront"
 aws cloudfront create-invalidation --distribution-id 'E2TDU4DXXIRA4F' --paths '/*'
 echo "Deployment is done"
